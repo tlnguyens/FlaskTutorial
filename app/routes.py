3d920d3d3e0d80426ab305b7cab6fa571c6ba5dc
Sébastien Nguyen <sebastien.nguyen@lsce.ipsl.fr>
2022-03-09 16:17:17 +0100

    added loop to parse blog messages

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Sébastien'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avangers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
